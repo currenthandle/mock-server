14fce8b9bd372cbec0105c804c590c91f04a1b4e
Casey <leptone55@gmail.com>
2023-01-07 16:44:26 -0800

    server working with cors

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const cors = require('cors');
 const express = require('express');
 const path = require('path');
 
-// const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client');
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 const app = express();
 
@@ -28,6 +28,28 @@ app.get('/text', (req, res) => {
 app.get('/api/users', async (req, res) => {
   const users = await prisma.user.findMany();
   res.json(users);
+});
+
+// post route for creating a new configuration with data passed from client
+app.post('/api/configuration', async (req, res) => {
+  console.log('req', req);
+  const configuration = await prisma.configuration.create({
+    data: {
+      name: 'test',
+      description: 'test',
+      color: 'test',
+    },
+  });
+  res.json(configuration);
+});
+
+// a get route for retrieving all configurations
+app.get('/api/configurations', async (req, res) => {
+  // const configurations = await prisma.configuration.findMany();
+  res.json({
+    name: 'casey',
+  });
+  // res.json(configurations);
 });
 
 app.listen(3001, () => {
